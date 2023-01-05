773b76c686e881ebe3a7d26282886f6ae3ab0a63
R5C-V15T6AMD5S <91406500+R5C-V15T6AMD5S@users.noreply.github.com>
2023-01-05 21:28:39 +0100

    Add files via upload

--- config/i3/shells/SingleMon.sh
+++ config/i3/shells/SingleMon.sh
@@ -9,7 +9,7 @@ killall conky
 #polybar --config=~/.config/polybar.old/config_nord i3bar &
 #polybar --config=~/.config/polybar.old/config_dracula i3bar &
 
-feh --bg-scale ~/Pictures/WallPP/Colorschemes/gruvbox/gruv-forest-5854B.jpg &
-#feh --bg-scale ~/Pictures/WallPP/Colorschemes/nord/nord1.jpg
+#feh --bg-scale ~/Pictures/WallPP/Colorschemes/gruvbox/gruv-forest-5854B.jpg &
+feh --bg-scale ~/Pictures/WallPP/Colorschemes/nord/nord1.jpg
 #feh --bg-scale ~/Pictures/WallPP/Colorschemes/dracula/fed.png
 #feh --bg-scale ~/Pictures/WallPP/Colorschemes/everforest/other/japanese_pedestrian_street_everforest.png
--- config/i3/shells/autostart.sh
+++ config/i3/shells/autostart.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
+# Compositor
+killall picom
+while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
+picom --config ~/.config/picom/picom.conf --vsync &
+
 # Some very needed apps
 autotiling &
 numlockx on &
+#volume listen "%i %v %p\n" &
 dunst -config ~/.config/dunst/dunstrc &
-xss-lock --transfer-sleep-lock -- i3lock -i /home/o5-13/Pictures/WallPP/Colorschemes/gruvbox/PixelForest.png -t --nofork &
+xss-lock --transfer-sleep-lock -- i3lock -i /home/o5-13/Pictures/WallPP/Colorschemes/nord/peng.png -t --nofork &
 i3-battery-popup -n -v 100 -L 20 -l 10 -s ~/.config/i3/audio/warning.wav &
 
 # NetworkManager is the most popular way to manage wireless networks on Linux,
