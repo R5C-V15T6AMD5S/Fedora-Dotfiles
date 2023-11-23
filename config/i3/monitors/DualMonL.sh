6171cc412da7e1a3a23b663a490c1f365181e620
R5C-V15T6AMD5S <91406500+R5C-V15T6AMD5S@users.noreply.github.com>
2023-11-23 20:53:18 +0100

    Minor changes to configs

--- config/i3/monitors/DualMonL.sh
+++ config/i3/monitors/DualMonL.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-xrandr --output eDP --mode 1920x1080 --primary --pos 0x0 --rate 60 --rotate normal --output HDMI-A-0 --mode 1680x1050 --pos 1680x0 --rotate normal --rate 60 &
+xrandr --output eDP --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal &
 
 #bash -c ~/.config/i3/scripts/polybar.sh
 
