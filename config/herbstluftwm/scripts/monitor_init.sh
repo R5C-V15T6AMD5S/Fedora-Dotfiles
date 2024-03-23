b5ec885587e462846faf367798cb9eb12a243a6b
R5C-V15T6AMD5S <91406500+R5C-V15T6AMD5S@users.noreply.github.com>
2024-03-23 20:02:56 +0100

    Small updates
    Added some new colorschemes and better control for dual monitors.

--- config/herbstluftwm/scripts/monitor_init.sh
+++ config/herbstluftwm/scripts/monitor_init.sh
@@ -4,9 +4,13 @@ extr=$(xrandr --query | grep '^HDMI-A-0')
 
 if [[ $extr = *\ connected* ]]; then
         herbstclient set_monitors 1920x1080+0+0 1920x1080+1920+0 &
+        notify-send "HDMI-A-0 CONNECTED" "Choose your Monitor setup . . ." & 
         bash -c ~/.scripts/monitors.sh &
 else
         herbstclient set_monitors 1920x1080+0+0 &
+        notify-send "HDMI-A-0 DISCONNECTED" "Running the Laptop setup . . ." &
         bash -c ~/.config/i3/monitors/SingleMon.sh  &
 fi
+
+sleep 5; notify-send "Welcome to herbstluftwm :)))" "Have a [REDACTED] day *wink*" &
 
