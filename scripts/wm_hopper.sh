1ca5149f3ff7a366c87a7b97bc26bc74af7c45de
Luka Capan <91406500+R5C-V15T6AMD5S@users.noreply.github.com>
2024-06-08 20:05:15 +0200

    Added new scripts
    New scripts for virtualbox, server & WMs.

--- scripts/wm_hopper.sh
+++ scripts/wm_hopper.sh
@@ -3,22 +3,24 @@
 # Options for wms
 i3wm="i3wm"
 hlwm="HLWM"
+bspwm="bspwm"
 
 # Get answer from user via rofi
-selected_option=$(echo "$i3wm
-$hlwm" | rofi -dmenu\
-                  -i\
-                  -p "WMs"\
-		  -theme "~/.config/rofi/themes/wms.rasi"
-    )
+selected_option=$(echo -e "$i3wm\n$hlwm\n$bspwm" | rofi -dmenu \
+                  -i \
+                  -p "WMs" \
+                  -theme "~/.config/rofi/themes/wms.rasi")
+
+# Trim whitespace from the selected option
+selected_option=$(echo "$selected_option" | xargs)
 
 # Do something based on selected option
-if [ "$selected_option" == "$i3wm" ]
-then
+if [ "$selected_option" == "$i3wm" ]; then
     i3
-elif [ "$selected_option" == "$hlwm" ]
-then
+elif [ "$selected_option" == "$hlwm" ]; then
     herbstluftwm
+elif [ "$selected_option" == "$bspwm" ]; then
+    bspwm
 else
     echo "No match"
 fi
