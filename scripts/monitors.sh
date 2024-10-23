5987c8b1009866a46193fbd4089291f27f8d809e
Luka Capan <91406500+R5C-V15T6AMD5S@users.noreply.github.com>
2024-10-23 18:07:40 +0200

    Added ALL .SCRIPTS files

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -22,22 +22,22 @@ $tvLivingRoom" | rofi -dmenu\
 # Do something based on selected option
 if [ "$selected_option" == "$laptop" ]
 then
-    bash -c /home/o5-13/.config/i3/monitors/SingleMon.sh
+    bash -c /home/o5-14/.config/i3/monitors/SingleMon.sh
 elif [ "$selected_option" == "$monRight" ]
 then
-    bash -c /home/o5-13/.config/i3/monitors/DualMonR.sh
+    bash -c /home/o5-14/.config/i3/monitors/DualMonR.sh
 elif [ "$selected_option" == "$monLeft" ]
 then
-    bash -c /home/o5-13/.config/i3/monitors/DualMonL.sh
+    bash -c /home/o5-14/.config/i3/monitors/DualMonL.sh
 elif [ "$selected_option" == "$tvRight" ]
 then
-    bash -c /home/o5-13/.config/i3/monitors/TVMonR.sh
+    bash -c /home/o5-14/.config/i3/monitors/TVMonR.sh
 elif [ "$selected_option" == "$tvLeft" ]
 then
-    bash -c /home/o5-13/.config/i3/monitors/TVMonL.sh
+    bash -c /home/o5-14/.config/i3/monitors/TVMonL.sh
 elif [ "$selected_option" == "$tvLivingRoom" ]
 then
-    bash -c /home/o5-13/.config/i3/monitors/LivingRoomTV.sh
+    bash -c /home/o5-14/.config/i3/monitors/LivingRoomTV.sh
 else
     echo "No match"
 fi
