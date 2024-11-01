1baff24e5ddc46a3cddc01994b4e018d61b1ebb7
Luka Capan <91406500+R5C-V15T6AMD5S@users.noreply.github.com>
2024-11-02 00:08:53 +0100

    Delete config/i3 directory

--- config/i3/scripts/info.sh
+++
@@ -1,7 +0,0 @@
-:set ff=unix
-#!/bin/sh
-
-conky -c ~/.config/conky/conky-top.conf &
-conky -c ~/.config/conky/conky-bottom.conf &
-
-feh --no-fehbg --bg-scale ~/Pictures/WallPP/Colorschemes/gruvbox/OS/fedora_center.png &
