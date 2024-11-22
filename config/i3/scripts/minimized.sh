0d9e41e820fac86225c3f4da534e5394ba771552
Luka Capan <91406500+R5C-V15T6AMD5S@users.noreply.github.com>
2024-11-22 23:34:50 +0100

    Delete config/i3 directory

--- config/i3/scripts/minimized.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# minimizes the currently focused window
-current_window=$(xdotool getactivewindow) && \
-echo $current_window >> ~/.cache/.minim && \
-xdotool windowunmap $current_window
